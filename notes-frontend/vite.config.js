4fc58ce6ffcd0c33aceb95570c63f5dae51017ed
Shaikh Abdulrauf Asifparvez <arauf0801@gmail.com>
2026-08-24 23:00:47 +0530

    updated icon

--- notes-frontend/vite.config.js
+++ notes-frontend/vite.config.js
@@ -20,17 +20,17 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'maskable-icon.png',
+            src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'maskable-icon.png',
+            src: 'pwa-192x192.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'maskable-icon.png',
+            src: 'maskable-new.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
